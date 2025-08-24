61e0c7300e93da790659ab33c21c69c5ea3aae0b
bre <bre@bres-MacBook-Air.local>
2025-08-24 15:50:39 +0800

    clean up

--- src/ui/articleview.cc
+++ src/ui/articleview.cc
@@ -921,22 +921,6 @@ void ArticleView::linkClickedInHtml( QUrl const & url_ )
   }
 }
 
-void findGdauUrls(const QString& html)
-{
-  // Regex to find gdau:// URLs inside any attribute
-  // This looks for something like attribute="gdau://..."
-  QRegularExpression regex(R"((\w+)=\"gdau://([^\"]+)\")");
-
-  QRegularExpressionMatchIterator i = regex.globalMatch(html);
-
-  while (i.hasNext()) {
-    QRegularExpressionMatch match = i.next();
-    QString attributeName = match.captured(1);  // e.g. href or data-src-mp3
-    QString gdauPath = match.captured(2);       // the path after gdau://
-
-    qDebug() << "Found gdau URL in attribute" << attributeName << ":" << gdauPath;
-  }
-}
 
 QString ArticleView::replaceTags(QString &html){
   auto ptags=std::make_shared<QVector<GdauTagInfo>>(  );
@@ -948,16 +932,6 @@ QString ArticleView::replaceTags(QString &html){
     id +=1;
     QRegularExpressionMatch match=i.next();
     QString fullTag=match.captured(0);
-    //QString url=match.captured(1);
-    /*
-    int beginPos=match.capturedStart(0);
-    int endPos=match.capturedEnd(0);
-    //cant use offset since multiple tags*/
-
-
-    //QString attributes = match.captured(2);
-
-    // Regex to extract gdau:// url inside the attributes string
     QRegularExpression urlRegex(R"(gdau://[^\"'\s>]+)");
     QRegularExpressionMatch urlMatch = urlRegex.match(fullTag);
 
@@ -967,7 +941,6 @@ QString ArticleView::replaceTags(QString &html){
     }
     qDebug() << urlString <<"\n";
 
-
     QUrl url(urlString);
     sptr<Dictionary::Class> dict = dictionaryGroup->getDictionaryById(url.host().toStdString());
     if (!dict) continue;
@@ -976,22 +949,7 @@ QString ArticleView::replaceTags(QString &html){
     AudioResource ar{"",false,req,std::make_shared<QMutex>(  )};
     ptags->append({id,fullTag,urlString,ar});
   }
-  //QVector<AudioResource> audioResources;
-  //QVector<sptr<Dictionary::DataRequest>> reqs;
-  //race=true;
-  /*
-  for (const auto &GdauTagInfo : tags) {
-    QString urlStr=GdauTagInfo.url;
-    QUrl url(GdauTagInfo.url);
-    sptr<Dictionary::Class> dict = dictionaryGroup->getDictionaryById(url.host().toStdString());
-    if (!dict) continue;
 
-    sptr<Dictionary::DataRequest> req = dict->getResource(url.path().mid(1).toUtf8().data());
-
-    AudioResource ar{ "", false, req, std::make_shared<QMutex>()};
-    audioResources.append(ar);
-  }
-  */
   auto tags=*ptags ;
   for (auto &tag: tags) {
     sptr<Dictionary::DataRequest> req=tag.resource.req;
@@ -999,30 +957,17 @@ QString ArticleView::replaceTags(QString &html){
     connect(req.get(), &Dictionary::Request::finished, this, [this, req, ptags, tag,html]() {
         onAudioRequestFinished(req, ptags, tag.id, html);
       });
-
   }
-  /*
-  QtConcurrent::run([audioResources]() {
-    onAllAudioResourcesReady(audioResources, html);
-  */
-  /*
-  connect(checkMechain,CM::allDone,this,[this,audioResources,html]->void {
-      return ;
-  });
-  */
 
   return QString();
 };
-
-
-
 
 void ArticleView::onAudioRequestFinished(sptr<Dictionary::DataRequest> req, std::shared_ptr<QVector<GdauTagInfo>> ptags,int tagid,QString html) {
   if (req->dataSize() > 0) {
     QByteArray data(req->getFullData().data(), static_cast<int>(req->dataSize()));
     QString base64Audio = data.toBase64();
 
-    // find resource in vector by url and update
+    // find resource in vector by id and update
     for (auto &tag : *ptags) {
       if (tag.id == tagid) {
         //tag.resource.mutex->lock();
@@ -1034,31 +979,15 @@ void ArticleView::onAudioRequestFinished(sptr<Dictionary::DataRequest> req, std:
     }
     onAllAudioResourcesReady(ptags,html);
 
-    /*
-    // Check if all finished:
-    if (!resourceLocked) {
-      resourceLocked=true;
-      bool allDone = std::all_of(resources.begin(), resources.end(), [](const AudioResource &r) { return r.finished; });
-      resourceLocked=false;
-      if (allDone && race) {
-        onAllAudioResourcesReady(resources,html);
-      }
-    }
-      */
   }
 }
 
 void ArticleView::onAllAudioResourcesReady(std::shared_ptr<QVector<GdauTagInfo>> ptags,QString &originalHtml) {
-  //let's use simple method
-  //use a spin lock
+  //return when any tag are unfinished
   for(auto & tag:*ptags) {
-      //tag.resource.mutex->lock();
       if (!tag.resource.finished) return;
-      //tag.resource.mutex->unlock();
   }
 
-  //QString modifiedHtml =originalHtml;
-  //race=false;
   auto tags=*ptags;
   for (const auto &tag : *ptags) {
     qDebug() << tag.resource.base64Data <<'\n' ;
@@ -1083,61 +1012,6 @@ void ArticleView::onAllAudioResourcesReady(std::shared_ptr<QVector<GdauTagInfo>>
   }
 
   sendToAnki(webview->title(), originalHtml, translateLine->text());
-}
-
-
-void findGdauTags(const QString& html)
-{
-  QRegularExpression tagRegex(R"(<(\w+)([^>]*?gdau://[^>]*?)>)");
-  QRegularExpressionMatchIterator i = tagRegex.globalMatch(html);
-
-  while (i.hasNext()) {
-    QRegularExpressionMatch match = i.next();
-    QString tagName = match.captured(1);   // e.g. "a"
-    QString fullTag = match.captured(0);   // the whole tag, e.g. <a ... >
-
-    qDebug() << "Found tag:" << tagName << "\nFull tag text:" << fullTag << "\n";
-  }
-}
-
-
-QString embedAudioBase64(QString html)
-{
-  // Regex to match gdau:// URLs in href or data-src-mp3 attributes
-
-  QRegularExpression regex(R"delim((href|data-src-mp3)="gdau://([^"]+)")delim");
-  QRegularExpressionMatchIterator i = regex.globalMatch(html);
-
-  while (i.hasNext()) {
-    QRegularExpressionMatch match = i.next();
-    QString attr = match.captured(1);    // href or data-src-mp3
-    QString resourcePath = match.captured(2); // path after gdau://
-
-    // Construct resource path according to your resource system
-    // For example, if resourcePath is "64b124f9ac24cfde2acffccb4523068c/media/english/breProns/ld41ai.mp3"
-    // you need to map this to your Qt resource or file path
-    QString qrcPath = QString(":/%1").arg(resourcePath); // Adjust this if needed
-
-    QFile file(qrcPath);
-    if (!file.open(QIODevice::ReadOnly)) {
-      qWarning() << "Failed to open resource:" << qrcPath;
-      continue;
-    }
-
-    QByteArray data = file.readAll();
-    QString base64Data = QString::fromLatin1(data.toBase64());
-
-    QString dataUrl = QString("data:audio/mpeg;base64,%1").arg(base64Data);
-
-    // Replace the old URL with data URL in html
-    // Note: Use exact match from captured text
-    QString oldUrl = QString("%1=\"gdau://%2\"").arg(attr, resourcePath);
-    QString newUrl = QString("%1=\"%2\"").arg(attr, dataUrl);
-
-    html.replace(oldUrl, newUrl);
-  }
-
-  return html;
 }
 
 
